fa6ef5d88608ec6ac2ced3199b5e2d88865bd114
Fernando Espinosa Victoriano <fespinosa@fespinosa.nt.core>
2018-05-14 13:02:40 -0400

    make Book component a controlled

--- src/Book.js
+++ src/Book.js
@@ -1,16 +1,35 @@
-import React, {Component} from 'react' 
-import BookShelfChanger from './BookShelfChanger'
+import React, { Component } from 'react'
 
-class Book extends Component{
+class Book extends Component {
 
-    render(){
+    state = {
+        optionValue: this.props.bookShelf
+    }
+
+    handleChange = (event) => {
+        this.setState({ optionValue: event.target.value });
+        this.props.onChangeShelf({
+            id: this.props.bookId,
+            shelf: this.props.bookShelf,
+            destShelf: event.target.value
+        })
+    }
+
+    render() {
+        console.log(this.state.options)
         return (
             <div className="book">
                 <div className="book-top">
-                    <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: `url("${this.props.image}")`}}></div>
-                    <BookShelfChanger  onChange={this.props.onChangeShelf} 
-                                       bookId={this.props.bookId} 
-                                       bookShelf={this.props.bookShelf}  />
+                    <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: `url("${this.props.image}")` }}></div>
+                    <div className="book-shelf-changer">
+                        <select value={this.state.optionValue} onChange={this.handleChange}>
+                            <option value="none" disabled>Move to...</option>
+                            <option value="currentlyReading">Currently Reading</option>
+                            <option value="wantToRead">Want to Read</option>
+                            <option value="read" >Read</option>
+                            <option value="none">None</option>
+                        </select>
+                    </div>
                 </div>
                 <div className="book-title">{this.props.title}</div>
                 <div className="book-authors">{this.props.authors}</div>
