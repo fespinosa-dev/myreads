c7a88f53964c808d81b123c12889bd07073713b8
Fernando Espinosa <fjevictoriano@gmail.com>
2019-03-18 11:12:32 -0400

    Change Router to HashRouter

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import MyReadApp from "./MyReadApp";
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 import "./index.css";
 
 ReactDOM.render(
-	<BrowserRouter>
+	<HashRouter>
 		<MyReadApp />
-	</BrowserRouter>,
+	</HashRouter>,
 	document.getElementById("root")
 );
